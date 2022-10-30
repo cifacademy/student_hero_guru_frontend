5e4062bc029cfb49ad70077f44db4727838f2fc8
fachris007 <fachrisajovi@gmail.com>
2022-10-30 18:56:01 +0700

    Creating register page, login page, onboarding page, and setting routes. Interface only

---
+++ Hero-Guru-student/src/App.js
@@ -0,0 +1,11 @@
+import { RouterDefault } from './Router/RouterDefault';
+
+function App() {
+  return (
+    <div className="App">
+      <RouterDefault />
+    </div>
+  );
+}
+
+export default App;
